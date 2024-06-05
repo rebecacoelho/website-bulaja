be6f3cc417400334225585fed50ccce3da0cbdd7
rebecacoelho <rebecasantoscoelho@gmail.com>
2024-06-05 00:36:38 -0300

    remove images

--- src/components/TabsMedicine/TabsMedicine.tsx
+++ src/components/TabsMedicine/TabsMedicine.tsx
@@ -8,14 +8,11 @@ export default function TabsMedicine(props: TabsMedicine) {
           <TabsTrigger className="text-lg" value="description">Descrição</TabsTrigger>
           <TabsTrigger className="text-lg" value="treatment">Tratamento</TabsTrigger>
           <TabsTrigger className="text-lg" value="side-effects">Efeitos Colaterais</TabsTrigger>
-          <TabsTrigger className="text-lg" value="precautions">Preocações</TabsTrigger>
-          <TabsTrigger className="text-lg" value="images">Imagens</TabsTrigger> 
-        </TabsList>
+          <TabsTrigger className="text-lg" value="precautions">Preocações</TabsTrigger>        </TabsList>
         <TabsContent className="mt-5" value="description">{props.descricao}</TabsContent>
         <TabsContent className="mt-5" value="treatment">{props.tratamento}</TabsContent>
         <TabsContent className="mt-5" value="side-effects">{props.efeitosColaterais}</TabsContent>
         <TabsContent className="mt-5" value="precautions">{props.precaucoes}</TabsContent>
-        <TabsContent className="mt-5" value="images">Teste 5</TabsContent>
       </Tabs>
     </div>
   );
